d47d73588b558d813999fca7f26829eba85f533a
jackc88 <jackc@jchen.org>
2026-03-08 21:01:14 -0400

    Update calendar_builder.py

--- calendar_builder.py
+++ calendar_builder.py
@@ -82,12 +82,15 @@ def build():
                 #ev.begin -= timedelta(hours=12)
                 ev.begin = ev.begin.shift(hours=-12)
                 logging.debug(f"Adj -12 hrs: {ev.begin}")
+                logging.debug(f"ERROR: start: {ev.begin!r} end: {ev.end!r}")
                 #return adj_ev.begin
                 return ev.begin
             else:
                 logging.debug(f"ERROR: start: {ev.begin} end: {ev.end}")
-                ev.begin -= timedelta(hours=12)
+                #ev.begin -= timedelta(hours=12)
+                #logging.debug(f"Adj -12 hrs: {ev.begin}")
                 logging.debug(f"Adj -12 hrs: {ev.begin}")
+                logging.debug(f"ERROR: start: {ev.begin!r} end: {ev.end!r}")
                 return ev.begin
 
             #match (ev.begin, ev.end):
