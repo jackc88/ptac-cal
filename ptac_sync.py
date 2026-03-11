1a30947ad7192e2f4c8515f8729d6c5269dae6b5
jackc88 <jackc@jchen.org>
2026-03-10 21:14:04 -0400

    Update ptac_sync.py

--- ptac_sync.py
+++ ptac_sync.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 # ==================== CONFIG ====================
 ADDRESS_MAP = {
-    "Denunzio": "DeNunzio Pool, 1000 University Place, Princeton, NJ 08544",
+    "Denunzio": "DeNunzio Pool, Stadium Dr, Princeton, NJ 08540",
     "WAC": "Windsor Athletic Club, 70 Palmer Drive, East Windsor, NJ 08520",
     "MCCC": "Mercer County Community College Pool, 1200 Old Trenton Road, West Windsor, NJ 08550",
     "Princeton MS": "Princeton Middle School Pool, 217 Walnut Lane, Princeton, NJ 08540",
